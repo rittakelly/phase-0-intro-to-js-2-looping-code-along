2813919a85229a407b78684071645ee659163c6c
Ritta <brittanykelly33@gmail.com>
2022-04-27 02:29:25 -0400

    commit tree 87659a79e96f92d1b857d6d001cd99d3e32a65a4

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const cards = [];
+
+function writeCards(name) {
+    for (let i = 0; i < name.length; i++) {
+        cards.message = `Thank you, ${name[i]}, for the wonderful surprise gift!`;
+        cards.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+        
+    }
+    return cards
+}
+
+function countDown() {
+    let countDown = 10
+    while (countDown >= 0) {
+        console.log(countDown--)
+    }
+}
